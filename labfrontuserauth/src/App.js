b2543acdca30687a18129af45d6368b184466854
Vance-M <myveryvills@yahoo.com>
2021-03-03 08:57:50 -0800

    pages and logic in

--- labfrontuserauth/src/App.js
+++ labfrontuserauth/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
+import Header from './Header.js';
+import Home from './Home.js';
+import SignUp from './Auth/signUpPage.js';
+import Login from './Auth/loginPage.js';
+import TodosList from './todos/toDoListPage.js';
+import { getUserFromLocalStorage, putUserInLocalStorage } from './localStorageUtils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    user: getUserFromLocalStorage()
+  }
+  handleUserChange = (user) => {
+    this.setState({ user })
+
+    putUserInLocalStorage(user);
+  }
+    render() {
+      return (
+          <div>
+              <Router>
+                <Header/>
+                  <Switch>
+                      <Route 
+                          path="/" 
+                          exact
+                          render={(routerProps) => <Home {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/todos" 
+                          exact
+                          render={(routerProps) => 
+                            <TodosList user={this.state.user} {...routerProps} />} 
+                      />
+                      <Route 
+                        path="/login" 
+                        exact
+                        render={(routerProps) => 
+                          <Login handleUserChange={this.handleUserChange} {...routerProps} />} 
+                      />
+                      <Route 
+                        path="/signup" 
+                        exact
+                        render={(routerProps) => 
+                          <SignUp handleUserChange={this.handleUserChange} {...routerProps} />} 
+                      />
+                  </Switch>
+              </Router>
+          </div>
+      )
+  }
 }
-
-export default App;
