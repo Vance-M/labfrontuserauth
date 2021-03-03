bfdc4940005d0c8a27e8f3b24a909fc4973755fe
Vance-M <myveryvills@yahoo.com>
2021-03-02 21:52:27 -0800

    pages made

---
+++ labfrontuserauth/src/Auth/signUpPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class signUpPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
